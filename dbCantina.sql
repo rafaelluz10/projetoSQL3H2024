aa64cff1b5cceb3f62300732aac76bc8b72e4e22
rafaelluz10 <rafael.luztp1@gmail.com>
2024-08-16 20:01:50 -0300

    mostrando dados nas tabelas

--- dbCantina.sql
+++ dbCantina.sql
@@ -63,6 +63,8 @@ primary key(codProd),
 foreign key(codForn) references tbFornecedores(codForn)
 );
 
+
+
 create table tbVendas(
 codVenda int not null auto_increment,
 dataVenda date,
@@ -75,6 +77,8 @@ primary key(codVenda),
 foreign key(codUsu)references tbUsuarios(codUsu),
 foreign key(codCli)references tbClientes(codCli),
 foreign key(codProd)references tbProdutos(codProd));
+
+
 
 -- visualizando a estrutura das tabelas
 desc tbFuncionarios;
@@ -96,12 +100,22 @@ insert into tbFornecedores(nome,email,cnpj)values('Magazine Luiza','magalu@gmail
 insert into tbClientes(nome,email,telCelular)values('Lais Lima','laislima@gmail.com','944890124');
 insert into tbClientes(nome,email,telCelular)values('Julia Maria','juhmah@outlook.com','955790124');
 insert into tbClientes(nome,email,telCelular)values('Marcela Trega','marcela2@gmail.com','977905344');
+insert into tbClientes(nome,email,telCelular)values('Denize Reis','denize2@gmail.com','977953434');
 
 insert into tbUsuarios(nome,senha,codFunc)values('jose.cianci','123456',1);
 insert into tbUsuarios(nome,senha,codFunc)values('marcela.trega','654321',2);
+
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Mesas Martinelli',15,850.00,'2024/10/10','2020/08/16','19:50:55',1);
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Brinquedo Chuqqy',10,20.000,'2024/05/05','2020/08/16','23:50:55',2);
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Bolo de Cenoura',100,10.00,'2024/12/25','2024/10/10','10:30:00',2);
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Martelo do Thor',1,150.00,'2099/05/20','2024/08/16','19:55:00',1);
+
+insert into tbVendas(dataVenda,horaVenda,quantidade,codUsu,codCli,codProd)values('2024/08/16','19:57:00',10,2,3,4);	
 
 -- visualizando os registros das tabelas -- 
 select * from tbFuncionarios; 
 select * from tbFornecedores; 
 select * from tbClientes;
 select * from tbUsuarios; 
+select * from tbProdutos; 
+select * from tbVendas; 
